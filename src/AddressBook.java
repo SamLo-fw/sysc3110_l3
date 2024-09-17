9433297c10c20ec288feb013988756195389c13a
SamLo-fw <124114997+SamLo-fw@users.noreply.github.com>
2024-09-17 14:50:28 -0400

    add addressbook class

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> addresses;
+    public AddressBook(){
+        this.addresses = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        this.addresses.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.addresses.remove(buddy);
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo();
+        AddressBook book = new AddressBook();
+        book.addBuddy(buddy);
+        book.removeBuddy(buddy);
+
+        System.out.printf("this is the AddressBook.");
+    }
+}
